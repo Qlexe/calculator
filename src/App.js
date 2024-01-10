1169b028af7896d6df3a788217e9e1f579d70e4e
Oleksandr Kapetii <105638501+Qlexe@users.noreply.github.com>
2024-01-10 02:45:49 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
         const newValue = Number(currentValue) * (Number(previousValue) / 100);
         setPreviousValue(newValue);
         setOutput(newValue);
-      } else{
+      } else {
         const newValue = Number(currentValue) / 100;
         setCurrentValue(newValue);
         setOutput(newValue);
@@ -77,85 +77,45 @@ function App() {
     <div className="App">
       <div className="App-output">
         <div className="App-output_header">
-          <img className="App-logo" src="calci.png" alt="logo calci"></img>
-          <a href="https://github.com/calci-lib">
+          <img className="App-logo" src="CALCI.png" alt="logo calci"></img>
+          <a href="">
             <img className="App-setting" src="setting.png" alt="setting"></img>
           </a>
         </div>
-        <div className="App-output_display">
-          {output}
-          {/* {!operator ? currentValue : previousValue} */}
-        </div>
+        <div className="App-output_display">{output}</div>
       </div>
       <div className="App-input">
         <table className="App-input_table">
           <tr>
-            <td onClick={handlerClick}>
-              C
-            </td>
-            <td onClick={handlerClick}>
-              %
-            </td>
-            <td onClick={handlerClick}>
-              /
-            </td>
-            <td
-              onClick={handlerClick}
-              style={{ fontWeight: 700 }}
-            >
+            <td onClick={handlerClick}>C</td>
+            <td onClick={handlerClick}>%</td>
+            <td onClick={handlerClick}>/</td>
+            <td onClick={handlerClick} style={{ fontWeight: 700 }}>
               x
             </td>
           </tr>
           <tr>
-            <td onClick={handlerClick}>
-              7
-            </td>
-            <td onClick={handlerClick}>
-              8
-            </td>
-            <td onClick={handlerClick}>
-              9
-            </td>
-            <td onClick={handlerClick}>
-              -
-            </td>
+            <td onClick={handlerClick}>7</td>
+            <td onClick={handlerClick}>8</td>
+            <td onClick={handlerClick}>9</td>
+            <td onClick={handlerClick}>-</td>
           </tr>
           <tr>
-            <td onClick={handlerClick}>
-              4
-            </td>
-            <td onClick={handlerClick}>
-              5
-            </td>
-            <td onClick={handlerClick}>
-              6
-            </td>
-            <td onClick={handlerClick}>
-              +
-            </td>
+            <td onClick={handlerClick}>4</td>
+            <td onClick={handlerClick}>5</td>
+            <td onClick={handlerClick}>6</td>
+            <td onClick={handlerClick}>+</td>
           </tr>
           <tr>
-            <td onClick={handlerClick}>
-              1
-            </td>
-            <td onClick={handlerClick}>
-              2
-            </td>
-            <td onClick={handlerClick}>
-              3
-            </td>
-            <td
-              rowSpan={2}
-              onClick={handlerClick}
-              style={{ fontSize: "50px" }}
-            >
+            <td onClick={handlerClick}>1</td>
+            <td onClick={handlerClick}>2</td>
+            <td onClick={handlerClick}>3</td>
+            <td rowSpan={2} onClick={handlerClick} style={{ fontSize: "50px" }}>
               =
             </td>
           </tr>
           <tr>
-            <td onClick={handlerClick}>
-              0
-            </td>
+            <td onClick={handlerClick}>0</td>
             <td
               id="decimal"
               onClick={handlerClick}
