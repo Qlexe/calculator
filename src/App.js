da41954190da1f40826232d148441ae3c0654114
Oleksandr Kapetii <oleksandrkapetii@outlook.com>
2024-01-10 04:36:23 +0200

    1

--- src/App.js
+++ src/App.js
@@ -7,51 +7,66 @@ function App() {
   const [operator, setOperator] = useState(null);
   const [output, setOutput] = useState(0);
 
-
   function handlerClick(e) {
     const value = e.target.textContent;
     if (value === "C") {
       setCurrentValue(null);
       setPreviousValue(null);
       setOperator(null);
-      setOutput(0)
+      setOutput(0);
       return;
     }
 
-    
-
-
     if ((Number(value) || value === "." || value === "0") && !operator) {
       console.log(value);
-      setCurrentValue(currentValue ? currentValue + value : value);
-      setOutput(currentValue);
+      const newValue = currentValue ? currentValue + value : value;
+      setCurrentValue(newValue);
+      setOutput(newValue);
     }
     if ((Number(value) || value === "." || value === "0") && operator) {
       console.log(value);
-      setPreviousValue(previousValue ? previousValue + value : value);
-      setOutput(previousValue);
+      const newValue = previousValue ? previousValue + value : value;
+      setPreviousValue(newValue);
+      setOutput(newValue);
     }
 
     if (value === "+" || value === "-" || value === "x" || value === "/") {
       setOperator(value);
+      setOutput(value);
     }
 
-    if (value === "%"){
-      setPreviousValue(Number(currentValue) * (Number(previousValue) / 100));
+    if (value === "%") {
+      if (previousValue) {
+        const newValue = Number(currentValue) * (Number(previousValue) / 100);
+        setPreviousValue(newValue);
+        setOutput(newValue);
+      } else{
+        const newValue = Number(currentValue) / 100;
+        setCurrentValue(newValue);
+        setOutput(newValue);
+      }
     }
 
     if (value === "=") {
       if (operator === "+") {
-        setCurrentValue(Number(currentValue) + Number(previousValue));
+        const newValue = Number(currentValue) + Number(previousValue);
+        setCurrentValue(newValue);
+        setOutput(newValue);
       }
       if (operator === "-") {
-        setCurrentValue(Number(currentValue) - Number(previousValue));
+        const newValue = Number(currentValue) - Number(previousValue);
+        setCurrentValue(newValue);
+        setOutput(newValue);
       }
       if (operator === "x") {
-        setCurrentValue(Number(currentValue) * Number(previousValue));
+        const newValue = Number(currentValue) * Number(previousValue);
+        setCurrentValue(newValue);
+        setOutput(newValue);
       }
       if (operator === "/") {
-        setCurrentValue(Number(currentValue) / Number(previousValue));
+        const newValue = Number(currentValue) / Number(previousValue);
+        setCurrentValue(newValue);
+        setOutput(newValue);
       }
       setPreviousValue(null);
       setOperator(null);
@@ -68,23 +83,23 @@ function App() {
           </a>
         </div>
         <div className="App-output_display">
-          {!operator ? currentValue : previousValue}
+          {output}
+          {/* {!operator ? currentValue : previousValue} */}
         </div>
       </div>
       <div className="App-input">
         <table className="App-input_table">
           <tr>
-            <td id="clear" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               C
             </td>
-            <td id="percent" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               %
             </td>
-            <td id="divide" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               /
             </td>
             <td
-              id="multiply"
               onClick={handlerClick}
               style={{ fontWeight: 700 }}
             >
@@ -92,45 +107,44 @@ function App() {
             </td>
           </tr>
           <tr>
-            <td id="seven" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               7
             </td>
-            <td id="eight" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               8
             </td>
-            <td id="nine" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               9
             </td>
-            <td id="subtract" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               -
             </td>
           </tr>
           <tr>
-            <td id="four" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               4
             </td>
-            <td id="five" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               5
             </td>
-            <td id="six" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               6
             </td>
-            <td id="add" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               +
             </td>
           </tr>
           <tr>
-            <td id="one" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               1
             </td>
-            <td id="two" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               2
             </td>
-            <td id="three" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               3
             </td>
             <td
-              id="equals"
               rowSpan={2}
               onClick={handlerClick}
               style={{ fontSize: "50px" }}
@@ -139,7 +153,7 @@ function App() {
             </td>
           </tr>
           <tr>
-            <td id="zero" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               0
             </td>
             <td
@@ -149,7 +163,7 @@ function App() {
             >
               .
             </td>
-            <td id="delete" onClick={handlerClick}>
+            <td onClick={handlerClick}>
               <img src="delete.png" alt="Delete"></img>
             </td>
           </tr>
